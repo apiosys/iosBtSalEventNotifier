cf434b896dfba95f4f4c787d0a2e8b3a335d0aba
Brandon Bodnar <brandon.bodnar@driversiti.com>
2015-10-28 16:48:13 -0400

    Move Event View handling code to Main Storyboard and to Event View Controller

---
+++ SALNotifier/Helpers/EventTags.h
@@ -0,0 +1,61 @@
+//
+//  EventTags.h
+//  SALNotifier
+//
+//  Created by Brandon Bodnar on 10/28/15.
+//  Copyright © 2015 Apio Systems. All rights reserved.
+//
+
+static NSString * const STOP_HB_TEXT = @"Stop HB";
+static NSString * const START_HB_TEXT = @"Start HB";
+
+static NSString * const STOP_RA_TEXT = @"Stop RA";
+static NSString * const START_RA_TEXT = @"Start RA";
+
+static NSString * const STOP_WK_TEXT = @"Stop Walking";
+static NSString * const START_WK_TEXT = @"Start Walking";
+
+static NSString * const STOP_VE_TEXT = @"Stop Veh. Entry";
+static NSString * const START_VE_TEXT = @"Start Veh. Entry";
+
+static NSString * const STOP_SP_TEXT = @"Stop Speeding";
+static NSString * const START_SP_TEXT = @"Start Speeding";
+
+static NSString * const STOP_VX_TEXT = @"Stop Veh. Exit";
+static NSString * const START_VX_TEXT = @"Start Veh. Exit";
+
+static NSString * const STOP_HTL_TEXT = @"Stop Hard-Left";
+static NSString * const START_HTL_TEXT = @"Start Hard-Left";
+
+static NSString * const STOP_HTR_TEXT = @"Stop Hard-Right";
+static NSString * const START_HTR_TEXT = @"Start Hard-Right";
+
+static NSString * const STOP_RSL_TEXT = @"Stop Rumble-Left";
+static NSString * const START_RSL_TEXT = @"Start Rumble-Left";
+
+static NSString * const STOP_RSR_TEXT = @"Stop Rumble-Right";
+static NSString * const START_RSR_TEXT = @"Start Rumble-Right";
+
+static NSString * const STOP_ABD_TEXT = @"Stop Airbag-Driver";
+static NSString * const START_ABD_TEXT = @"Start Airbag-Driver";
+
+static NSString * const STOP_ABP_TEXT = @"Stop Airbag-Pass.";
+static NSString * const START_ABP_TEXT = @"Start Airbag-Pass.";
+
+static NSString * const STOP_DS_TEXT = @"Stop Door Slam";
+static NSString * const START_DS_TEXT = @"Start Door Slam";
+
+static NSString * const STOP_LCL_TEXT = @"Stop Ln Chg-Left";
+static NSString * const START_LCL_TEXT = @"Start Ln Chg-Left";
+
+static NSString * const STOP_LCR_TEXT = @"Stop Ln Chg-Right";
+static NSString * const START_LCR_TEXT = @"Start Ln Chg-Right";
+
+static NSString * const STOP_ADVERTISING_TEXT = @"Stop Advertising";
+static NSString * const START_ADVERTISING_TEXT = @"Start Advertising";
+
+static NSString * const STOP_ICYROAD_TEXT = @"Stop Icy Road";
+static NSString * const START_ICYROAD_TEXT = @"Start Icy Road";
+
+static NSString * const STOP_SNOWYROAD_TEXT = @"Stop Snow Road";
+static NSString * const START_SNOWYROAD_TEXT = @"Start Snow Road";
